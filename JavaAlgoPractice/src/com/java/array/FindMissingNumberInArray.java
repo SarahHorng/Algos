2b48fd1cdaa8042bd91def4aa85a6cd3840d3d6c
Sarah.Horng <Sarah.Horng@TRNNYC30006.fdmgroup.local>
2019-10-02 16:27:46 -0400

    search algorithms

--- JavaAlgoPractice/src/com/java/array/FindMissingNumberInArray.java
+++ JavaAlgoPractice/src/com/java/array/FindMissingNumberInArray.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class FindMissingNumberInArray {
 
-	static int[] numbers = {1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 12, 13, 15};
+	static int[] numbers = { 1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 12, 13, 15 };
 
 	void findMissing(int list[]) {
 		int length = list.length;
-		int expectedsum = (length +1) * (length + 2) / 2;
+		int expectedsum = (length + 1) * (length + 2) / 2;
 
 		int sum = 0;
 		for (int i = 0; i < length; i++) {
@@ -20,7 +20,6 @@ public class FindMissingNumberInArray {
 	}
 
 	public static void main(String[] args) {
-
 		System.out.println(Arrays.toString(numbers));
 		FindMissingNumberInArray mn = new FindMissingNumberInArray();
 		mn.findMissing(numbers);
