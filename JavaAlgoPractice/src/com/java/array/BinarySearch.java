08df6ec46e8c776315c728b93000df531f937df6
Sarah.Horng <Sarah.Horng@TRNNYC30006.fdmgroup.local>
2019-10-02 16:31:43 -0400

    binary search

--- JavaAlgoPractice/src/com/java/array/BinarySearch.java
+++ JavaAlgoPractice/src/com/java/array/BinarySearch.java
@@ -2,31 +2,31 @@ package com.java.array;
 
 public class BinarySearch {
 
-	public static void binarySearch(int arr[], int first, int last, int key) {
+	public int binarySearch(int[] inputArr, int key) {
 
-		int mid = (first + last) / 2;
+		int start = 0;
+		int end = inputArr.length - 1;
 
-		while (first <= last) {
-			if (arr[mid] < key) {
-				first = mid + 1;
-			} else if (arr[mid] == key) {
-				System.out.println("Element is found at index: " + mid);
-				break;
-			} else {
-				last = mid - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (key == inputArr[mid]) {
+				return mid;
 			}
-			mid = (first + last) / 2;
+			if (key < inputArr[mid]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
 		}
-		if (first > last) {
-			System.out.println("Element is not found!");
-		}
+		return -1;
 	}
 
-	public static void main(String args[]) {
-		int arr[] = { 10, 20, 30, 40, 50 };
-		int first =0;
-		int last = arr.length - 1;		
-		int key = 30;
-		binarySearch(arr, first, last, key);
+	public static void main(String[] args) {
+
+		BinarySearch mbs = new BinarySearch();
+		int[] arr = { 2, 4, 6, 8, 10, 12, 14, 16 };
+		System.out.println("Key 14's position: " + mbs.binarySearch(arr, 14));
+		int[] arr1 = { 6, 34, 78, 123, 432, 900 };
+		System.out.println("Key 432's position: " + mbs.binarySearch(arr1, 432));
 	}
 }
